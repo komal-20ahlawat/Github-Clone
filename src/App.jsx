d004cb76b1d032585346cb3ca80432a632b76410
komal <ahlawatkomal20@gmail.com>
2026-04-06 16:39:35 +0530

    vv

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,14 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0d1117] text-white">
       
-      {/* Top Navbar */}
       <Navbar />
 
-      {/* Main Layout */}
       <div className="grid grid-cols-[350px_1fr_320px]">
         
-        {/* Left Sidebar */}
         <Sidebar />
 
-        {/* Center Content */}
         <Middle />
 
-        {/* Right Panel */}
         <RightSIde />
 
       </div>
